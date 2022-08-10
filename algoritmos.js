6252ff95b6eb2579c8089c3d9806342abc777ff7
Vanesa Arce <arce.vanesaruth@gmail.com>
2022-08-10 17:28:54 -0300

    estructuras de control proyecto

---
+++ algoritmos.js
@@ -0,0 +1,31 @@
+/* function suma () {
+    var a = 4;
+    var b = 2;
+
+    var resultado = a + b;
+
+    if (resultado > 3){
+        return console.log ("El resultado es mayor a 3");
+
+    } else {
+        return console.log ("El resultado es menor a 3");
+}
+}
+
+suma();
+ */
+function resta () {
+    var a = 5;
+    var b = 5;
+    var resultado = a - b;
+    if ( resultado === 0) {
+        return console.log ("logrado");
+        
+    }else {
+        return console.log ("no logrado");
+
+    }
+        
+    }
+    resta()
+    
